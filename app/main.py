288fbdaa6eed0ed5d5e0e21fff6d3250712fb4ba
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-01-12 15:02:16 +0100

    changed semantic and moved logic to func

--- app/main.py
+++ app/main.py
@@ -9,18 +9,17 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    people_obj_list = []
-
     {person["name"]: Person(person["name"], person["age"])
      for person in people}
 
-    for person in people:
-        person_objt = Person.people[person["name"]]
+    return [
+        add_spouse(person) for person in people
+    ]
 
-        if person.get("wife") and person.get("wife") is not None:
-            person_objt.wife = Person.people[person["wife"]]
-        elif person.get("husband") and person.get("husband") is not None:
-            person_objt.husband = Person.people[person["husband"]]
-        people_obj_list.append(person_objt)
-
-    return people_obj_list
+def add_spouse(person) -> object:
+    person_obj = Person.people[person["name"]]
+    if person.get("wife") and person.get("wife") is not None:
+        person_obj.wife = Person.people[person["wife"]]
+    elif person.get("husband") and person.get("husband") is not None:
+        person_obj.husband = Person.people[person["husband"]]
+    return person_obj
